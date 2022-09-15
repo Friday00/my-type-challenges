85c621274e927a75ccb6fbdc610491ba70f3cae0
顾国威 <1427698733@qq.com>
2022-09-15 23:03:13 +0800

    feat: MyPicker

---
+++ picker.ts
@@ -0,0 +1,27 @@
+export type MyPicker<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+interface ITestValue {
+  name: string;
+  age: number;
+  nickname: string;
+  test2?: ITestValue2;
+}
+
+interface ITestValue2 {
+  name: string;
+  age: number;
+  nickname: string;
+}
+
+const value: ITestValue = {
+  name: "张三",
+  age: 33,
+  nickname: "法外狂徒",
+};
+
+// const textValue: MyPicker<ITestValue, "age"> = "123";
+const textValue: MyPicker<ITestValue, "age"> = { age: 11 };
+
+const textValue2: MyPicker<ITestValue, "test2"> = value;
